6a4cb64344ebc57043a1edb91f6b9378c56dcd34
Gavin <licheng@titanwind.com.cn>
2022-05-01 23:26:01 +0800

    [ADD]微信消息发送

---
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/msg/wxwork/WeiChat.java
@@ -0,0 +1,25 @@
+package com.ruoyi.common.utils.msg.wxwork;
+
+/**
+ * @author Gavin Li
+ * @group CoderBean
+ * @date 2022/1/6 11:43
+ * @desc 企业微信管理后台：https://work.weixin.qq.com/wework_admin/frame#apps
+ */
+public class WeiChat {
+    public static void main(String[] args) {
+        WeChatMsgSend swx = new WeChatMsgSend();
+        try {
+            String token = swx.getToken("ww19b1921d75b9a23d","f9iMFCTMqmVGOrvLaNxytGunHfMMZ9S0nJ-QC0a0Am0");
+            String postdata = swx.createpostdata("LiCheng", "text",
+                    1000002, "content","这是一条测试信息");
+            String resp = swx.post("utf-8", WeChatMsgSend.CONTENT_TYPE,(new WeChatUrlData()).getSendMessage_Url(), postdata, token);
+            System.out.println("获取到的token======>" + token);
+            System.out.println("请求数据======>" + postdata);
+            System.out.println("发送微信的响应数据======>" + resp);
+        }catch (Exception e) {
+            e.getStackTrace();
+
+        }
+    }
+}
